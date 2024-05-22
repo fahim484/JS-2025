f7180d47ec89ba092275e70780fcb03f8de36f7c
Muhammad Fahim Faysal <76892541+fahim484@users.noreply.github.com>
2024-05-22 05:10:07 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // <--- Syntax // treat all JS conde as newer JS version
+
+// alert(3 + 3) // we are useing nodejs not borwser
+
+console.log(3
+     + 
+     3) // code readability should be high(not use)
+
+console.log("Fahim") // (use this)
+
+// JS DataTypes:
+let name = "Fahim"
+let age = 19
+let isLoggedIn = false
+let state;
+let fun = null;
+
+
+/* Primitive DataTypes : 
+1. number (2 to power 53)(bigint), 
+2. string ("" or ''),
+3. boolean (true or false),
+4. null (standalone value),
+5. undefined
+6. symbol (unique)
+*/
+
+
+// object
+
+console.log(typeof "fahim");
+console.log(typeof age);
+console.log(typeof null); // object // interview question
+console.log(typeof undefined); // undefined
